0a6f9cebe3bb87472ac506fc2e9a5c061e013920
Chenxing Luo <chenxing.luo@gmail.com>
2022-02-08 23:27:53 -0500

    Fix plugin installation

--- build.sh
+++ build.sh
@@ -2,6 +2,13 @@ CWD=$(pwd)
 
 pip3 install -r requirements.txt
 
-cd plugins ; ls | xargs pip3 install -Ue ; cd $CWD
+cd plugins
+
+for plugin in $(ls)
+do
+    pip3 install -Ue ./$plugin
+done
+
+cd $CWD
 
 python3 -m mkdocs build
